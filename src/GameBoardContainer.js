3cd43dc2057d8949a867321dd71140ed1142c12b
Your Name <you@example.com>
2019-07-30 09:05:09 -0400

    added start game button

--- src/GameBoardContainer.js
+++ src/GameBoardContainer.js
@@ -3,13 +3,19 @@ import Countdown from 'react-countdown-now';
 import Completionist from './Completionist'
 
 class GameBoardContainer extends React.Component{
-
+  componentDidMount(){
+    fetch("http://jservice.io/api/random?count=100")
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
    render(){
       return (
          <div>
          <Countdown className="column" date={Date.now() + 600000}>
             <Completionist />
          </Countdown>
+
+
 
 
          </div>
